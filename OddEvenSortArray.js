a28e05b755ab522d39c0f64852cfeb320af6faa1
KubantsevAS <92636316+KubantsevAS@users.noreply.github.com>
2022-02-07 01:39:13 +0300

    Add files via upload

---
+++ OddEvenSortArray.js
@@ -0,0 +1,9 @@
+// сортировка нечётных и чётных элементов массива
+
+function sortOddEven(array){
+	const oddArray = array.split(' ').filter(item => item % 2).sort((a, b) => a - b);
+  const evenArray = array.split(' ').filter(item => !(item % 2)).sort((a, b) => a- b);
+	return [...oddArray, ...evenArray];
+  }
+let inputNum = prompt('Введите массив чисел', );
+alert(sortOddEven(inputNum));
